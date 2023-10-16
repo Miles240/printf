06861f5802442af41f08f44fab0818d18bb49c15
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-16 17:10:07 +0100

    test

--- _printf.c
+++ _printf.c
@@ -1,53 +1,51 @@
 #include "main.h"
 
 /**
- * 
-*/
+ * _printf - prints a formatted string to the stdout
+ * @format: ...
+ *
+ * Return: formatted string
+ */
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+	char c;
+	char *str;
 	int chars_printed = 0;
+	va_list args;
+
+	va_start(args, format);
 
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			format++; // Move past the '%'
+			format++;
 			switch (*format)
 			{
 			case 'c':
-				char c = va_arg(args, int);
-				write(1, &c, 1); // Write the character to stdout
+				c = va_arg(args, int);
+				write(1, &c, 1);
 				chars_printed++;
 				break;
 			case 's':
-				char *str = va_arg(args, char *);
-				write(1, str, strlen(str)); // Write the string to stdout
+				str = va_arg(args, char *);
+				write(1, str, strlen(str));
 				chars_printed += strlen(str);
 				break;
 			case '%':
-				write(1, "%", 1); // Write a '%' character
+				write(1, "%", 1);
 				chars_printed++;
 				break;
 			}
 		}
 		else
 		{
-			write(1, format, 1); // Write other characters as is
+			write(1, format, 1);
 			chars_printed++;
 		}
 		format++;
 	}
-
 	va_end(args);
-	return chars_printed;
-}
-
-int main()
-{
-	int printed_chars = _printf("Hello, %c! This is a %s example. This is a %% character.\n", 'W', "printf");
-	printf("Total characters printed: %d\n", printed_chars);
-	return 0;
+	return (chars_printed);
 }
--- main.h
+++ main.h
@@ -6,5 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _printf(const char *format, ...);
 
 #endif
