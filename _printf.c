4d61b1435149310a0cb335776d33a839b0438f36
ayuure <bertinaayuure0147@gmail.com>
2023-10-16 17:04:49 +0000

    1

--- _printf.c
+++ _printf.c
@@ -9,9 +9,8 @@
 
 int _printf(const char *format, ...)
 {
-	char c;
-	char *str;
-	int chars_printed = 0;
+	char c, *str, num_str[12];
+	int chars_printed = 0, num, len;
 	va_list args;
 
 	va_start(args, format);
@@ -36,6 +35,19 @@ int _printf(const char *format, ...)
 			case '%':
 				write(1, "%", 1);
 				chars_printed++;
+				break;
+			
+			case 'd':
+				num = va_arg(args, int);
+				len = sprintf(num_str, "%d", num);
+				write(1, num_str, len);
+				chars_printed += len;
+				break;
+			case 'i':
+				num = va_arg(args, int);
+				len = sprintf(num_str, "%d", num);
+				write(1, num_str, len);
+				chars_printed += len;
 				break;
 			}
 		}
