5e91dab42c6dc79f6e2c04e0e4294864222b93b9
Miles <134848387+Miles240@users.noreply.github.com>
2023-10-17 01:45:10 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,107 +1,115 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
-{
-	char c, *str, num_str[12], binary_str[33];
-	int chars_printed = 0, num, len, buffer_index = 0;
-	unsigned int num2;
-	va_list args;
-	char buffer[1024]; // Local buffer
+/*
+* print_char - prints a formatted character
+*prin
+*/
 
-	va_start(args, format);
+int print_char(char c) {
+    char buffer[2];
+    buffer[0] = c;
+    buffer[1] = '\0';
+    write(1, buffer, 1);
+    return 1;
+}
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				c = va_arg(args, int);
-				buffer[buffer_index++] = c;
-				chars_printed++;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				while (*str != '\0')
-				{
-					buffer[buffer_index++] = *str;
-					str++;
-					chars_printed++;
-				}
-				break;
-			case '%':
-				buffer[buffer_index++] = '%';
-				chars_printed++;
-				break;
-			case 'd':
-			case 'i':
-				num = va_arg(args, int);
-				len = sprintf(num_str, "%d", num);
-				for (int i = 0; i < len; i++)
-				{
-					buffer[buffer_index++] = num_str[i];
-				}
-				chars_printed += len;
-				break;
-			case 'u':
-				num2 = va_arg(args, unsigned int);
-				len = sprintf(num_str, "%u", num2);
-				for (int i = 0; i < len; i++)
-				{
-					buffer[buffer_index++] = num_str[i];
-				}
-				chars_printed += len;
-				break;
-			case 'o':
-				num2 = va_arg(args, unsigned int);
-				len = sprintf(num_str, "%o", num2);
-				for (int i = 0; i < len; i++)
-				{
-					buffer[buffer_index++] = num_str[i];
-				}
-				chars_printed += len;
-				break;
-			case 'x':
-				num2 = va_arg(args, unsigned int);
-				len = sprintf(num_str, "%x", num2);
-				for (int i = 0; i < len; i++)
-				{
-					buffer[buffer_index++] = num_str[i];
-				}
-				chars_printed += len;
-				break;
-			case 'X':
-				num2 = va_arg(args, unsigned int);
-				len = sprintf(num_str, "%X", num2);
-				for (int i = 0; i < len; i++)
-				{
-					buffer[buffer_index++] = num_str[i];
-				}
-				chars_printed += len;
-				break;
-			}
-		}
-		else
-		{
-			buffer[buffer_index++] = *format;
-			chars_printed++;
-		}
+int print_string(const char *str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    write(1, str, len);
+    return len;
+}
 
-		if (buffer_index >= 1024)
-		{
-			write(1, buffer, buffer_index);
-			buffer_index = 0;
-		}
+int print_integer(int num) {
+    char num_str[12];
+    int len = sprintf(num_str, "%d", num);
+    write(1, num_str, len);
+    return len;
+}
 
-		format++;
-	}
+int print_unsigned(unsigned int num) {
+    char num_str[12];
+    int len = sprintf(num_str, "%u", num);
+    write(1, num_str, len);
+    return len;
+}
 
-	if (buffer_index > 0)
-	{
-		write(1, buffer, buffer_index);
-	}
-	va_end(args);
-	return chars_printed;
+int print_octal(unsigned int num) {
+    char num_str[12];
+    int len = sprintf(num_str, "%o", num);
+    write(1, num_str, len);
+    return len;
+}
+
+int print_hex(unsigned int num, int uppercase) {
+    char num_str[12];
+    int len;
+    if (uppercase) {
+        len = sprintf(num_str, "%X", num);
+    } else {
+        len = sprintf(num_str, "%x", num);
+    }
+    write(1, num_str, len);
+    return len;
+}
+
+int my_printf(const char *format, ...) {
+    int chars_printed = 0;
+    va_list args;
+    char buffer[1024];
+    int buffer_index = 0;
+    
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    chars_printed += print_char(va_arg(args, int));
+                    break;
+                case 's':
+                    chars_printed += print_string(va_arg(args, char *));
+                    break;
+                case 'd':
+                case 'i':
+                    chars_printed += print_integer(va_arg(args, int));
+                    break;
+                case 'u':
+                    chars_printed += print_unsigned(va_arg(args, unsigned int));
+                    break;
+                case 'o':
+                    chars_printed += print_octal(va_arg(args, unsigned int));
+                    break;
+                case 'x':
+                    chars_printed += print_hex(va_arg(args, unsigned int), 0);
+                    break;
+                case 'X':
+                    chars_printed += print_hex(va_arg(args, unsigned int), 1);
+                    break;
+                case '%':
+                    chars_printed += print_char('%');
+                    break;
+            }
+        } else {
+            buffer[buffer_index++] = *format;
+            chars_printed++;
+        }
+
+        if (buffer_index >= 1024) {
+            write(1, buffer, buffer_index);
+            buffer_index = 0;
+        }
+
+        format++;
+    }
+
+    if (buffer_index > 0) {
+        write(1, buffer, buffer_index);
+    }
+    
+    va_end(args);
+    return chars_printed;
 }
